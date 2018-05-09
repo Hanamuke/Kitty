1ed8b1e94c4bbd157a7ad0a465640a985333dd96
Antoine Houdayer <antoine.houdayer@laposte.net>
2018-05-09 17:06:41 +0200

    Mate in 1 detection
    If there is a Mate in 1, plays it.
    
    Elo difference: 190.85 +/- 41.51
    SPRT: llr 2.95, lbound -2.94, ubound 2.94 - H1 was accepted

--- src/search.cpp
+++ src/search.cpp
@@ -9,6 +9,21 @@ void Thread::search() {
   Moves moves(&pos), moves_opponent;
   if (idx != 0)
     return;
+  ValueMove m;
+  while ((m = moves.next_move())) {
+    pos.do_move(m);
+    moves_opponent.set_pos(&pos);
+    if (pos.in_check() && moves_opponent.size() == 0) {
+      std::cout << Sync::lock << "bestmove "
+                << (MovePrint)(Move)(moves.size() == 0 ? NULL_MOVE
+                                                       : m ? m : *moves.begin())
+                << std::endl
+                << Sync::unlock;
+      return;
+    }
+    pos.undo_move();
+  }
+
   std::shuffle(moves.begin(), moves.end(), gen);
   std::cout << Sync::lock << "bestmove " << (MovePrint)(Move)(*moves.begin())
             << std::endl
