db50c62712cfb109bc8ec594a1a6f2120b245425
Hanamuke <antoine.houdayer@laposte.net>
2019-09-15 10:15:34 +0200

    Jouer les captures si possible

--- src/search.cpp
+++ src/search.cpp
@@ -5,19 +5,16 @@
 #include <algorithm>
 #include <iostream>
 
-void Thread::search()
-{
+void Thread::search() {
 
   Moves moves(&pos), moves_opponent;
   if (idx != 0)
     return;
   ValueMove m;
-  while ((m = moves.next_move()))
-  {
+  while ((m = moves.next_move())) {
     pos.do_move(m);
     moves_opponent.set_pos(&pos);
-    if (pos.in_check() && moves_opponent.size() == 0)
-    {
+    if (pos.in_check() && moves_opponent.size() == 0) {
       std::cout << Sync::lock << "bestmove "
                 << (MovePrint)(Move)(moves.size() == 0 ? NULL_MOVE
                                                        : m ? m : *moves.begin())
@@ -26,6 +23,14 @@ void Thread::search()
       return;
     }
     pos.undo_move();
+    if (pos.piece(get_to(m)) != NO_PIECE) {
+      std::cout << Sync::lock << "bestmove "
+                << (MovePrint)(Move)(moves.size() == 0 ? NULL_MOVE
+                                                       : m ? m : *moves.begin())
+                << std::endl
+                << Sync::unlock;
+      return;
+    }
   }
 
   std::shuffle(moves.begin(), moves.end(), gen);
