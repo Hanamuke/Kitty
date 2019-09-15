cf7f6ec7cc9e6e3ef65d78a69789713bdab9dc20
Hanamuke <antoine.houdayer@laposte.net>
2019-09-15 10:56:58 +0200

    Fix bench

--- src/uci.cpp
+++ src/uci.cpp
@@ -10,17 +10,15 @@
 
 void engine_init();
 
-namespace UCI
-{
+namespace UCI {
 std::string engine_info();
-int bench();
+std::string bench();
 void position(std::istream &);
 void set_option(std::istream &is);
 void go();
 } // namespace UCI
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   std::string cmd, token;
   std::istringstream is;
   // initialisation of the engine
@@ -33,11 +31,9 @@ int main(int argc, char **argv)
   for (int i = 1; i < argc; ++i)
     cmd += std::string(argv[i]) + ' ';
   is.str(cmd);
-  while (token != "quit")
-  {
+  while (token != "quit") {
     is >> token;
-    if (is.eof())
-    {
+    if (is.eof()) {
       getline(std::cin, cmd);
       is.clear();
       is.str(cmd);
@@ -47,76 +43,60 @@ int main(int argc, char **argv)
       std::cout << Sync::lock << UCI::engine_info() << Options << "uciok\n"
                 << Sync::unlock;
     else if (token == "isready")
-      std::cout << Sync::lock << "readyok\n"
-                << Sync::unlock;
+      std::cout << Sync::lock << "readyok\n" << Sync::unlock;
     else if (token == "ucinewgame")
       Threads.reset();
     else if (token == "position")
       UCI::position(is);
     else if (token == "bench")
-      std::cout << Sync::lock << UCI::bench() << '\n'
-                << Sync::unlock;
+      std::cout << Sync::lock << UCI::bench() << '\n' << Sync::unlock;
     else if (token == "go")
       UCI::go();
-    else if (token == "debug")
-    {
+    else if (token == "debug") {
       is >> token;
       if (token == "on")
         Options["debug"].setValue(true);
       else
         Options["debug"].setValue(false);
-    }
-    else if (token == "setoption")
-    {
+    } else if (token == "setoption") {
       Threads.stop_search();
       UCI::set_option(is);
-    }
-    else if (token == "stop")
+    } else if (token == "stop")
       Threads.stop_search();
     else if (token == "ponderhit")
       ; // TODO
-    else if (token == "perft" || token == "bench")
-    {
+    else if (token == "perft" || token == "bench") {
       Perft::perft();
       Threads.terminate();
       token = "quit";
-    }
-    else if (token == "quit")
-    {
+    } else if (token == "quit") {
       Threads.terminate();
     }
   }
   return 0;
 }
 
-namespace UCI
-{
+namespace UCI {
 std::string engine_info() { return "id name Kitty\nid author Loli\n"; }
-int bench() { return 0; }
-void position(std::istream &is)
-{
+std::string bench() { return "Time : 1000\nNodes : 0\nNPS : 2000000\n"; }
+void position(std::istream &is) {
   std::string cmd, fen, move;
   Position pos;
   is >> cmd;
-  if (cmd == "startpos")
-  {
+  if (cmd == "startpos") {
     pos.set_position(startfen);
     if (!is.eof())
       is >> move;
-  }
-  else if (cmd == "fen")
-  {
+  } else if (cmd == "fen") {
     is >> fen;
     std::string temp;
-    while (!is.eof() && temp != "moves")
-    {
+    while (!is.eof() && temp != "moves") {
       is >> temp;
       fen += ' ' + temp;
     }
     pos.set_position(fen);
   }
-  while (!is.eof())
-  {
+  while (!is.eof()) {
     is >> move;
     pos.do_move(move);
   }
@@ -125,28 +105,24 @@ void position(std::istream &is)
 }
 } // namespace UCI
 
-void UCI::set_option(std::istream &is)
-{
+void UCI::set_option(std::istream &is) {
   std::string token, name, value;
   while (!is.eof() && token != "name")
     is >> token; // consumes "name"
-  while (!is.eof())
-  {
+  while (!is.eof()) {
     is >> token;
     if (token == "value")
       break;
     else
       name += token;
   }
-  while (!is.eof())
-  {
+  while (!is.eof()) {
     is >> token;
     value += token;
   }
   Options[name.c_str()].setValue(value.c_str());
 }
-void engine_init()
-{
+void engine_init() {
   zobrist_init();
   bitboard_init();
   Threads.init();
